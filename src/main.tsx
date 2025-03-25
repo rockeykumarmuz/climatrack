160587532746ebb8c95a3226d8d8aa95942822cc
Rockey Kumar <rockey.kumar@samco.in>
2025-03-25 20:40:27 +0530

    changes browserrouter to  hashrouter to handle client side redirect

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/theme-provider.tsx'
 import Layout from './components/Layout.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -23,14 +23,14 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
+			<HashRouter>
 				<ThemeProvider defaultTheme='dark'>
 					<Layout>
 						<App />
 					</Layout>
 					<Toaster richColors />
 				</ThemeProvider>
-			</BrowserRouter>
+			</HashRouter>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</StrictMode>
