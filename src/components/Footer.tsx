88cdd17b0d2239a05d89437a456cc4f343fcdce3
Rockey Kumar <rockey.kumar@samco.in>
2025-03-25 20:25:49 +0530

    Changed foooter style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
-		<footer className='border-t-0 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-			<div className='container mx-auto text-center px-4 text-gray-400'>
+		<footer className='border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12'>
+			<div className='container mx-auto text-center px-4 text-gray-200'>
 				<p>Made with ❤️ by Rockey Kumar.</p>
 			</div>
 		</footer>
